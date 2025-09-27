40ec45c681984b5d8146e39e6c785696bb0ec9fe
HoTienQuoc <hotienquoc0429@gmail.com>
2025-09-27 11:28:08 +0700

    Update dependencies: add dcblogdev/laravel-box and laravel/sanctum; enable admin routes in API

--- laravel_microservices/Modules/Admin/routes/api.php
+++ laravel_microservices/Modules/Admin/routes/api.php
@@ -4,8 +4,8 @@ use Illuminate\Support\Facades\Route;
 use Modules\Admin\Http\Controllers\AdminController;
 use Modules\Admin\Http\Controllers\UserController;
 
-// Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
-//     Route::apiResource('admins', AdminController::class)->names('admin');
-// });
+Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
+    Route::apiResource('admins', AdminController::class)->names('admin');
+});
 
-Route::get("/",[UserController::class,'index']);
+Route::get("/user",[UserController::class,'index']);
